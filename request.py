73972f18481721819a83f644fa27f056f85118d1
Ryan Rasi <29680273+RyanRasi@users.noreply.github.com>
2024-01-25 20:17:22 +0000

    Python api request commit

---
+++ request.py
@@ -0,0 +1,23 @@
+import requests
+import io
+from dotenv import dotenv_values
+
+def fetch(location):
+    # Load variables from .env file
+    env_vars = dotenv_values(".env")
+
+    # Access variables
+    API_KEY = env_vars.get("API_KEY")
+
+    API_DAY_RANGE = 365
+    API_LOCATION = location
+    API_REQUEST = f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{API_LOCATION}/last{API_DAY_RANGE}days?include=days&key={API_KEY}&contentType=csv"
+    print(API_REQUEST)
+
+    response = requests.get(API_REQUEST)
+                    
+    if response.status_code != 200:
+        print('Request failed\nStatus code: ', response.status_code)
+        exit()
+    else:
+        return io.StringIO(response.text)
