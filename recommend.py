2631cf6cb75a4f6ae2c7dfc422176bba2b4a75d3
Ryan Rasi <29680273+RyanRasi@users.noreply.github.com>
2024-01-30 21:13:46 +0000

    Django commit

--- recommend.py
+++
@@ -1,7 +0,0 @@
-from tensorflow.keras.models import load_model
-
-# Load the saved model
-model = load_model('model.h5')
-
-# Use the loaded model for predictions or further training
-predictions = model.predict(X_test)
